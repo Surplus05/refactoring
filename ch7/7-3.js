bba0b34b1533dc033441e4c4ed300437616d565a
Surplus05 <104773096+Surplus05@users.noreply.github.com>
2022-08-11 17:42:18 +0900

    Update 7-3.js

--- ch7/7-3.js
+++ ch7/7-3.js
@@ -1,15 +1,42 @@
 export class Order {
   constructor(data) {
-    this.priority = data.priority;
+    this.priority = new Priority(data.priority);
+  }
+
+  isHighPriority() {
+    return this.priority.higherThan(new Priority("normal"));
+  }
+}
+
+class Priority {
+  #value;
+  constructor(value) {
+    if (Priority.legalValues().includes(value)) {
+      this.#value = value;
+    } else {
+      throw new Error(`${value} is invalie for Priority`);
+    }
+  }
+
+  get index() {
+    return Priority.legalValues().indexOf(this.#value);
+  }
+
+  higherThan(other) {
+    return this.index > other.index;
+  }
+
+  static legalValues() {
+    return ["low", "normal", "high", "rush"];
   }
 }
 
 const orders = [
-  new Order({ priority: 'normal' }),
-  new Order({ priority: 'high' }),
-  new Order({ priority: 'rush' }),
+  new Order({ priority: "normal" }),
+  new Order({ priority: "high" }),
+  new Order({ priority: "rush" }),
 ];
 
-const highPriorityCount = orders.filter(
-  (o) => 'high' === o.priority || 'rush' === o.priority
-).length;
+const highPriorityCount = orders.filter((o) => o.isHighPriority()).length;
+
+console.log(highPriorityCount);
