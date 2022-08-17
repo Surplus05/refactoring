e1963f4bbba8a5e9fa9a0db33834695b86c40f9e
Surplus05 <104773096+Surplus05@users.noreply.github.com>
2022-08-17 14:21:22 +0900

    ch9

--- ch9/9-5.js
+++ ch9/9-5.js
@@ -1,7 +1,13 @@
+const customerRepository = new CustomerRepository();
+
+const order = new Order(
+  data.number,
+  customerRepository.registerCustomer(data.customerId)
+);
 class Order {
-  constructor(data) {
-    this._number = data.number;
-    this._customer = new Customer(data.customerId);
+  constructor(number, custmoer) {
+    this._number = number;
+    this._customer = custmoer;
   }
 
   get customer() {
@@ -18,3 +24,26 @@ class Customer {
     return this._id;
   }
 }
+
+class CustomerRepository {
+  #customers;
+
+  constructor() {
+    this.#customers = new Map();
+  }
+
+  registerCustomer(id) {
+    if (!this.#customers.has(id)) {
+      this.#customers.set(id, new Customer(id));
+    }
+    return fundCustomer(id);
+  }
+
+  findCustomer(id) {
+    return this.#customers.get(id);
+  }
+}
+
+// 불변성은 좋으나 데이터가 변화할 때 마다 인스턴스를 만드는 경우 갱신이 어려울 수 있음.
+// 이러한 경우 인스턴스보다는 참조를 사용하자.
+// 고유한 id 별로 하나의 인스턴스만 만들어지는것을 보장하는 경우 리포지토리 패턴을 사용하는것이 좋다.
