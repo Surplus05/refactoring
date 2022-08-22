abb2fe6d8c648b623c721cfa62ba34ab40e2ac3d
Surplus05 <104773096+Surplus05@users.noreply.github.com>
2022-08-22 18:07:24 +0900

    ch10

--- ch10/10-6.js
+++ ch10/10-6.js
@@ -1,8 +1,14 @@
+import { strict as assert } from "node:assert";
+
 class Customer {
   constructor() {
     this.discountRate = 0;
   }
+
   applyDiscount(number) {
+    assert(number >= 0);
     return this.discountRate ? number - this.discountRate * number : number;
   }
 }
+
+new Customer().applyDiscount(-1);
