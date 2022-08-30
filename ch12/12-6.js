04b60c0670fbd62213d491c64cb9346bb3066b11
Surplus05 <104773096+Surplus05@users.noreply.github.com>
2022-08-30 16:57:05 +0900

    ch12

--- ch12/12-6.js
+++ ch12/12-6.js
@@ -1,26 +1,54 @@
 class Employee {
   #name;
-  #type;
-  constructor(name, type) {
-    this.validateType(type);
+  constructor(name) {
     this.#name = name;
     this.#type = type;
   }
 
-  validateType(arg) {
-    if (!['engineer', 'manager', 'salesperson'].includes(arg)) {
-      throw new Error(`${arg}라는 직원 유형은 없습니다.`);
-    }
-  }
-
   get type() {
-    return this.#type;
+    return "employee";
   }
 
   toString() {
     return `${this.#name} (${this.type})`;
   }
+
+  // static validateType(arg) {
+  //   if (!["engineer", "manager", "salesperson"].includes(arg)) {
+  //     throw new Error(`${arg}라는 직원 유형은 없습니다.`);
+  //   }
+  // }
+
+  static createEmployee(name, type) {
+    // Employee.validateType(type);
+    switch (type) {
+      case "engineer":
+        return new Engineer(name, type);
+      case "manager":
+        return new Manager(name, type);
+      case "salesperson":
+        return new Salesperson(name, type);
+      default:
+        throw new Error(`${arg}라는 직원 유형은 없습니다.`);
+    }
+  }
 }
 
-const ellie = new Employee('엘리', 'engineer');
-const bob = new Employee('밥', 'manager');
+class Engineer extends Employee {
+  get type() {
+    return "engineer";
+  }
+}
+class Manager extends Employee {
+  get type() {
+    return "manager";
+  }
+}
+class Salesperson extends Employee {
+  get type() {
+    return "salesperson";
+  }
+}
+
+const ellie = new Engineer("엘리", "engineer");
+const bob = new Manager("밥", "manager");
