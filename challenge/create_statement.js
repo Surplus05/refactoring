4b60bcf80c8d281fdf8f68b8a3a482743ae07bcd
Surplus05 <104773096+Surplus05@users.noreply.github.com>
2022-09-16 21:05:33 +0900

    end

---
+++ challenge/create_statement.js
@@ -0,0 +1,82 @@
+class Performance {
+  #audience;
+  #play;
+  constructor(audience, play) {
+    this.#audience = audience;
+    this.#play = play;
+  }
+
+  get audience() {
+    return this.#audience;
+  }
+
+  get play() {
+    return this.#play;
+  }
+
+  static create(audience, play) {
+    switch (play.type) {
+      case "tragedy":
+        return new Tragedy(audience, play);
+      case "comedy":
+        return new Comedy(audience, play);
+      default:
+        throw new Error(`unknown type : ${play.type}`);
+    }
+  }
+}
+
+class Tragedy extends Performance {
+  get amount() {
+    let result = 40000;
+    if (this.audience > 30) {
+      result += 1000 * (this.audience - 30);
+    }
+    return result;
+  }
+
+  get credits() {
+    return Math.max(this.audience - 30, 0);
+  }
+}
+class Comedy extends Performance {
+  get amount() {
+    let result = 30000;
+    if (this.audience > 20) {
+      result += 10000 + 500 * (this.audience - 20);
+    }
+    result += 300 * this.audience;
+    return result;
+  }
+
+  get credits() {
+    return Math.max(this.audience - 30, 0) + Math.floor(this.audience / 5);
+  }
+}
+
+export class Statement {
+  #customer;
+  #performances;
+
+  constructor(invoice, plays) {
+    this.#customer = invoice.customer;
+    this.#performances = invoice.performances.map((perf) =>
+      Performance.create(perf.audience, plays[perf.playID])
+    );
+  }
+
+  get totalAmount() {
+    return this.#performances.reduce((sum, perf) => (sum += perf.amount), 0);
+  }
+  get totalCredits() {
+    return this.#performances.reduce((sum, perf) => (sum += perf.credits), 0);
+  }
+
+  get customer() {
+    return this.#customer;
+  }
+
+  get performances() {
+    return [...this.#performances];
+  }
+}
